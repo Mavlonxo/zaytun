4441dbd090bb892f2bcc6d7a8cfea2ee3534496c
Mavlonxo <mavlonxon333@gmail.com>
2025-01-03 16:24:21 +0500

    ok

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './App.css'
+
+import bir from './img/bir.jpg'
+import ikki from './img/ikki.jpg'
+import uch from './img/uch.jpg'
+import tort from './img/tort.jpg'
+import besh from './img/besh.jpg'
+import olti from './img/olti.jpg'
+import yetti from './img/yetti.jpg'
+import sakkiz from './img/sakkiz.jpg'
+import toqqiz from './img/toqqiz.jpg'
+import on from './img/on.jpg'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='part'>
+       <img src={bir} alt=''/>
+       <img src={ikki} alt=''/>
+       <img src={uch} alt=''/>
+       <img src={tort} alt=''/>
+       <img src={besh} alt=''/>
+       <img src={olti} alt=''/>
+       <img src={yetti} alt=''/>
+       <img src={sakkiz} alt=''/>
+       <img src={toqqiz} alt=''/>
+       <img src={on} alt=''/>
+
+        
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
