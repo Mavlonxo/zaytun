01a634403c6ab4a1ac676dc254e4e3a16664a501
Mavlonxo <mavlonxon333@gmail.com>
2025-01-04 15:25:42 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -12,6 +12,36 @@ import sakkiz from './img/sakkiz.jpg'
 import toqqiz from './img/toqqiz.jpg'
 import on from './img/on.jpg'
 
+
+
+import a11 from './img/a11.jpg'
+import a12 from './img/a12.jpg'
+import a13 from './img/a13.jpg'
+import a14 from './img/a14.jpg'
+import a15 from './img/a15.jpg'
+import a16 from './img/a16.jpg'
+import a17 from './img/a17.jpg'
+import a18 from './img/a18.jpg'
+import a19 from './img/a19.jpg'
+import a20 from './img/a20.jpg'
+import a21 from './img/a21.jpg'
+import a22 from './img/a22.jpg'
+import a23 from './img/a23.jpg'
+import a24 from './img/a24.jpg'
+import a25 from './img/a25.jpg'
+import a26 from './img/a26.jpg'
+import a27 from './img/a27.jpg'
+import a28 from './img/a28.jpg'
+import a29 from './img/a29.jpg'
+import a30 from './img/a30.jpg'
+import a31 from './img/a31.jpg'
+import a32 from './img/a32.jpg'
+import a33 from './img/a33.jpg'
+import a34 from './img/a34.jpg'
+import a35 from './img/a35.jpg'
+import a36 from './img/a36.jpg'
+import a37 from './img/a37.jpg'
+
 function App() {
   return (
     <div className='container'>
@@ -26,6 +56,36 @@ function App() {
        <img src={sakkiz} alt=''/>
        <img src={toqqiz} alt=''/>
        <img src={on} alt=''/>
+
+       <img src={a11} alt=''/>
+       <img src={a12} alt=''/>
+       <img src={a13} alt=''/>
+       <img src={a14} alt=''/>
+       <img src={a15} alt=''/>
+       <img src={a16} alt=''/>
+       <img src={a17} alt=''/>
+       <img src={a18} alt=''/>
+       <img src={a19} alt=''/>
+       <img src={a20} alt=''/>
+       <img src={a21} alt=''/>
+       <img src={a22} alt=''/>
+       <img src={a23} alt=''/>
+       <img src={a24} alt=''/>
+       <img src={a25} alt=''/>
+       <img src={a26} alt=''/>
+       <img src={a27} alt=''/>
+       <img src={a28} alt=''/>
+       <img src={a29} alt=''/>
+       <img src={a30} alt=''/>
+       <img src={a31} alt=''/>
+       <img src={a32} alt=''/>
+       <img src={a33} alt=''/>
+       <img src={a34} alt=''/>
+       <img src={a35} alt=''/>
+       <img src={a36} alt=''/>
+       <img src={a37} alt=''/>
+
+       
 
         
 
